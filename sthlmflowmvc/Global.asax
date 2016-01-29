4895592c29c149938f01410c57a706c1100b635c
dendya01 <dyallo@live.se>
2016-01-29 20:33:28 +0100

    Sthlm Flow Mvc Project

---
+++ sthlmflowmvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sthlmflowmvc.MvcApplication" Language="C#" %>
---
+++ sthlmflowmvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sthlmflowmvc.Startup))]
+namespace sthlmflowmvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
